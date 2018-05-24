38f01b932b48f75650ad26ca45205716fd999e22
Danik <danik1492@gmail.com>
2018-05-24 16:42:16 +0300

    first commit

---
+++ src/main/java/com/smarthouse/smarthouse/SmartHouseApplication.java
@@ -0,0 +1,12 @@
+package com.smarthouse.smarthouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHouseApplication.class, args);
+	}
+}
